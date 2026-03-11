c9c01a33d06627485fc8e19326b5e9c518ed81c4
Mao Xiaoyi <maoxiaoyi423@163.com>
2026-03-11 14:51:44 +0800

    update

--- methods/baseline/__init__.py
+++ methods/baseline/__init__.py
@@ -6,5 +6,6 @@ from .logme import LogME
 from .vega import VEGAScorer, compute_vega_score
 from .vega_perfect import VEGAPerfectScorer, compute_vega_score_perfect
 from .vega_calibrated import VEGACalibratedScorer, compute_vega_score_calibrated
+from .vega_snr import VEGASNRScorer, compute_vega_score_snr
 
-__all__ = ['LogME', 'VEGAScorer', 'compute_vega_score', 'VEGAPerfectScorer', 'compute_vega_score_perfect', 'VEGACalibratedScorer', 'compute_vega_score_calibrated']
+__all__ = ['LogME', 'VEGAScorer', 'compute_vega_score', 'VEGAPerfectScorer', 'compute_vega_score_perfect', 'VEGACalibratedScorer', 'compute_vega_score_calibrated', 'VEGASNRScorer', 'compute_vega_score_snr']
