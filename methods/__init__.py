ee09dd0d1fe6148932b65122050d1667c1c3e8b1
Mao Xiaoyi <maoxiaoyi423@163.com>
2026-03-12 17:32:28 +0800

    update

--- methods/__init__.py
+++ methods/__init__.py
@@ -2,8 +2,12 @@
 Methods module for VLM model selection.
 Contains baseline methods and our proposed method.
 """
-
 from .baseline.logme import LogME
-from .baseline.vega_v2 import VEGAScorer, compute_vega_score
+from .baseline.vega_v3 import VEGAv3Scorer, compute_vega_v3_score
+from .baseline.vega_v2 import VEGAOptimizedScorer, compute_vega_score_optimized
+from .baseline.vega_v1 import VEGAOriginalScorer, compute_vega_score_original
 
-__all__ = ['LogME', 'VEGAScorer', 'compute_vega_score']
+
+
+__all__ = ['LogME', 'VEGAv3Scorer', 'compute_vega_v3_score', 'VEGAOptimizedScorer', 'compute_vega_score_optimized', 'VEGAOriginalScorer', 'compute_vega_score_original']
+    
--- methods/baseline/__init__.py
+++ methods/baseline/__init__.py
@@ -3,6 +3,7 @@ Baseline methods for VLM model selection.
 """
 
 from .logme import LogME
+from .vega_v3 import VEGAv3Scorer, compute_vega_v3_score
 from .vega_v2 import VEGAOptimizedScorer, compute_vega_score_optimized
 from .vega_v1 import VEGAOriginalScorer, compute_vega_score_original
 
@@ -11,5 +12,5 @@ __all__ = [
     'LogME',
     'VEGAOptimizedScorer', 'compute_vega_score_optimized',
     'VEGAOriginalScorer', 'compute_vega_score_original',
-    'VEGASNRScorer', 'compute_vega_score_snr'
+    'VEGAv3Scorer', 'compute_vega_v3_score',
 ]
